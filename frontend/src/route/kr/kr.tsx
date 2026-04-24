f7be46ace59efe26e00de97035a46164f325510e
devshortman <cowork.2025dv@gmail.com>
2026-04-24 17:28:22 +0900

    Update kr.tsx

--- frontend/src/route/kr/kr.tsx
+++ frontend/src/route/kr/kr.tsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import './style.css';
 import ShortCard from '../../component/short-card/ShortCard';
-import header from '../../component/header/header';
-import footer from '../../component/footer/footer';
+import Header from '../../component/header/header';
+import Footer from '../../component/footer/footer';
 import { API_ENDPOINTS } from '../../config/api';
 
 import parent from '../../assets/image/parent.svg';
@@ -83,7 +83,7 @@ const Kr = () => {
 
     return (
         <div id='kr'>
-            <header />
+            <Header />
             <div className='body'>
                 <div className='ca'>
                     <div>{title}</div>
@@ -144,7 +144,7 @@ const Kr = () => {
                     <div className="empty">데이터가 없습니다.</div>
                 )}
             </div>
-            <footer />
+            <Footer />
         </div>
     );
 };
